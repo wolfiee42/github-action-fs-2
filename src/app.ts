2b4d1c2cd31b0c0f3c9f207b0089ca080246f8e4
wolfiee42 <saifalislam2022@gmail.com>
2025-09-19 19:06:03 +0600

    fix: adding logger to check server logger

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get<object, MessageResponse>("/", (req, res) => {
-  // console.log("Ping pong!!");
+  console.log("Ping pong!!");
   
   res.status(200).json({
     message: "🦄🌈✨👋🌎🌏✨🌈🦄🦄🌈✨👋🌎🌏✨🌈🦄",
