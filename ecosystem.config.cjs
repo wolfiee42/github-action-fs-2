cca637c3b83a02289fb39c4adddab25eac97f7bd
saif <saif.digipylot@gmail.com>
2025-09-22 11:51:48 +0600

    fix: set default port to 8001 in application configuration

---
+++ ecosystem.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    apps : [
+        {
+          name: "express-api-starter-ts",
+          script: "npm start",
+          port: 8001 // optional, if have port set in app
+        }
+    ]
+  }
